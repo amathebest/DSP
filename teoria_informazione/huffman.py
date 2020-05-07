70e5c97f36934115af165059c0195512d8b203b4
amathebest <amatori.matt@gmail.com>
2020-05-07 19:41:43 +0200

    Encoding function with huffman algorithm works

--- teoria_informazione/huffman.py
+++ teoria_informazione/huffman.py
@@ -1,27 +1,20 @@
 
 
 class Node():
-    id = 0
     xi = ""
     pi = 0
+    ci = ""
 
-    def __init__(self, id, xi, pi):
-        self.id = id
+    def __init__(self, xi, pi):
         self.xi = xi
         self.pi = pi
         pass
 
     def __str__(self):
-        return "xi: " + self.xi + ", pi: " + str(self.pi)
+        return "xi: " + self.xi + ", pi: " + str(self.pi) + ", ci: " + self.ci
 
-
-class Edge():
-    label = ""
-    starting_node = 0
-    ending_node = 0
-
-    def __init__():
-        pass
+    def append_encoding(self, symbol):
+        self.ci = symbol + self.ci
 
 
 # function that creates the code with optimal lenght for a given alphabet X and probability distribution p
@@ -34,18 +27,41 @@ def encode():
 
     nodes = []
 
+    # creating the nodes with the values from file
+    for value in values:
+        curr_xi, curr_pi = value.split(" ")[0], float(value.split(" ")[1])
+        new_node = Node(curr_xi, curr_pi)
+        nodes.append(new_node)
 
-    for idx, value in enumerate(values):
-        curr_xi, curr_pi = value.split(" ")[0], value.split(" ")[1]
-        new_node = Node(idx, curr_xi, curr_pi)
+    og_nodes = nodes.copy()
+
+    # looping until there is a single node left. on each loop the two minimum pi are selected and a
+    # father node is created. meanwhile the code for each original node gets updated by appending a 0 or a 1
+    while len(nodes) >= 2:
+        # processing first node with minimum pi
+        node_1 = min(nodes, key = lambda x: x.pi)
+        if len(node_1.xi) > 1:
+            for elem in node_1.xi:
+                node_to_edit = [x for x in og_nodes if x.xi == elem][0]
+                node_to_edit.append_encoding("0")
+        node_1.append_encoding("0")
+        nodes = [x for x in nodes if x.xi != node_1.xi]
+
+        # processing second node with minimum pi
+        node_2 = min(nodes, key = lambda x: x.pi)
+        if len(node_2.xi) > 1:
+            for elem in node_2.xi:
+                node_to_edit = [x for x in og_nodes if x.xi == elem][0]
+                node_to_edit.append_encoding("1")
+        node_2.append_encoding("1")
+        nodes = [x for x in nodes if x.xi != node_2.xi]
+
+        new_node = Node(node_1.xi + node_2.xi, node_1.pi + node_2.pi)
         nodes.append(new_node)
 
 
-
-    for node in nodes:
+    for node in og_nodes:
         print(node)
-
-    
 
     return
 
@@ -64,11 +80,8 @@ def main():
     # d = decode mode
     mode = "e"
 
-
-
     if mode == "e":
         encode()
-
     else:
         decode()
 
